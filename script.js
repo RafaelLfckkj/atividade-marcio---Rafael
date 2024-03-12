af8955231b1516c60c4b3de878efd9165ce1ad05
rafael <rafaxolive@gmail.com>
2024-03-11 21:01:54 -0300

    quinto

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const botaoAbrir = document.querySelector("header > button")
 const botaoFechar = document.querySelector("header nav button")
 const body = document.querySelector("body")
+const nav = document.querySelector("header .mobile")
